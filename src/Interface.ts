4e10f89a9997c0c90a7f2a3afccbbf0bf728fb5b
BattoJutsu <175562675+Battojutsu@users.noreply.github.com>
2024-08-25 21:27:06 -0500

    stopping point

--- src/Interface.ts
+++ src/Interface.ts
@@ -3,12 +3,29 @@ import { QMainWindow, QPushButton, FlexLayout, QWidget, QPixmap, QLabel, QFileDi
 
 export class Interface {
 	window: QMainWindow;
+	central_widget: QWidget;
 
-	constructor() {
-
+	constructor(title: string) {
+		this.build_widget();
+		this.build_window(title);
 	}
 
 	show_window() {
 		this.window.show();
+	}
+
+	build_window(title: string) {
+		this.window = new QMainWindow();
+
+		this.window.setWindowTitle(title);
+
+		global.nodegui_window = this.window;
+	}
+
+	build_widget() {
+		this.central_widget = new QWidget();
+		this.central_widget.setObjectName("central_widget");
+		const size = new QSize(640, 480);
+		this.central_widget.setBaseSize(size);
 	}
 }
--- src/UserInterface.ts
+++ src/UserInterface.ts
@@ -1,60 +1,26 @@
 // This file is used to abstract some of the setup.
 import {
-	QMainWindow,
-	QPushButton,
-	FlexLayout,
-	QWidget,
-	QPixmap,
-	QLabel,
 	QFileDialog,
 	FileMode,
-	QSize,
 } from "@nodegui/nodegui";
 import * as fs from "fs";
-import { Interface } from "./Interface";
+import { FileEditor } from "./FileEditor";
 
-export class UserInterface extends Interface {
-	loader_button: QPushButton;
-	central_widget: QWidget;
+export class UserInterface extends FileEditor {
 	style_sheet: string;
-	image: QPixmap;
-	image_label: QLabel;
 
 	constructor() {
-		super();
-		this.build_loader_btn();
-		this.build_image_label();
-		this.build_central_widget();
-		this.build_window();
+		super("Sprite Sheet Animator");
 		this.build_style_sheet();
+		this.display_file_layout();
+		this.add_button_action();
 	}
 
 	build_style_sheet() {
 		this.style_sheet = fs.readFileSync(`${__dirname}/style.css`).toString();
 	}
 
-	build_image_label() {
-		this.image_label = new QLabel();
-		this.image = new QPixmap();
-		this.image_label.setPixmap(this.image);
-	}
-
-	build_layout(): FlexLayout {
-		const layout: FlexLayout = new FlexLayout();
-		return layout;
-	}
-
-	build_window() {
-		this.window = new QMainWindow();
-		this.window.setWindowTitle("Sprite Sheet Animator");
-		this.window.setCentralWidget(this.central_widget);
-		global.nodegui_window = this.window;
-	}
-
-	build_loader_btn() {
-		this.loader_button = new QPushButton();
-		this.loader_button.setText("Button");
-
+	add_button_action() {
 		this.loader_button.addEventListener("clicked", () => {
 			const fileDialog = new QFileDialog();
 			fileDialog.setFileMode(FileMode.AnyFile);
@@ -64,18 +30,6 @@ export class UserInterface extends Interface {
 
 			this.load_tileset(selectedFiles[0]);
 		});
-		this.loader_button.setObjectName("loader_btn");
-	}
-
-	build_central_widget() {
-		this.central_widget = new QWidget();
-		this.central_widget.setObjectName("central_widget");
-		const root_layout = this.build_layout();
-		this.central_widget.setLayout(root_layout);
-		root_layout.addWidget(this.loader_button);
-		root_layout.addWidget(this.image_label);
-		const size = new QSize(640, 480);
-		this.central_widget.setBaseSize(size);
 	}
 
 	load_tileset(image_url: string) {
