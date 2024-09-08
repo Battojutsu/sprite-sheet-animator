f5f0be28fb90b72f8ec329b0cac52d8411a02ce2
BattoJutsu <175562675+Battojutsu@users.noreply.github.com>
2024-09-08 14:24:33 -0500

    Configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,36 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  includeIgnoreFile(gitignorePath),
+  {ignores: [".config/*", "eslint.config.mjs"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': [
+        "error",
+        { allowIIFEs: true },
+      ],
+      '@typescript-eslint/consistent-return': "error",
+      "@typescript-eslint/no-unnecessary-condition": "error"
+    }
+  },
 ];
