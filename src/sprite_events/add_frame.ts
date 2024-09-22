bf72f0ee1234bb94a988eee2031827744537a933
BattoJutsu <175562675+Battojutsu@users.noreply.github.com>
2024-09-22 15:18:35 -0500

    Build animation structure

--- src/sprite_events/add_frame.ts
+++ src/sprite_events/add_frame.ts
@@ -1,8 +1,36 @@
+import { Coordinate } from "data_structures/proto/Coordinate";
+import { Frame } from "data_structures/Frame";
+import { Area } from "data_structures/proto/Area";
+import { AnimationStructure } from "data_structures/AnimationStructure";
+import { SpriteSheetEditor } from "interface/interface";
+
 /**
  * 
  * @param editor the editor calling this event.
  */
 export function add_frame() {
+	// TODO update th
 
-	console.log("todo");
+	const editor: SpriteSheetEditor = global.editor;
+	const anime: AnimationStructure = global.anime;
+	const default_time: number = editor.widgets.getDefaultTimeInput();
+
+	if(editor.selected_frame) {
+		// Source cordinate definition.
+		const s_coord = new Coordinate(
+			editor.selected_frame.x * editor.widgets.getWidthInput(),
+			editor.selected_frame.y * editor.widgets.getHeightInput()
+		)
+
+		// Source area definition
+		const s_area = new Area(editor.widgets.getWidthInput(), editor.widgets.getHeightInput(), "pixel");
+
+		// Define frame
+		const this_frame = new Frame(s_coord, s_area, default_time);
+
+		// Add the frame information to the animation.
+		anime.current_animation.frames.push(this_frame)
+	}
+
+	editor.widgets.frame_editor.update();
 }
